22237047c9092aa77f31cf012a9b010fb65bdb6f
asking28 <asking28@gmail.com>
2020-10-08 17:28:14 +0530

    bbb

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ function myFunction() {
   var dots = document.getElementById("dots");
   var moreText = document.getElementById("more");
   var btnText = document.getElementById("myBtn");
+  
+  var dots1 = document.getElementById("dots1");
+  var moreText1 = document.getElementById("more1");
+  var btnText1 = document.getElementById("myBtn1");
+  
 
   if (dots.style.display === "none") {
     dots.style.display = "inline";
@@ -12,7 +17,14 @@ function myFunction() {
     btnText.innerHTML = "Read less";
     moreText.style.display = "inline";
   }
-}
-function mul(a, b) {
-  return a * b;
+  
+  if (dots1.style.display === "none") {
+    dots1.style.display = "inline";
+    btnText1.innerHTML = "Read more";
+    moreText1.style.display = "none";
+  } else {
+    dots1.style.display = "none";
+    btnText1.innerHTML = "Read less";
+    moreText1.style.display = "inline";
+  }
 }
