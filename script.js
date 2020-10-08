2f2cff1302b7b2b9e5ceb9a28dce7838a8ad0b08
asking28 <asking28@gmail.com>
2020-10-08 17:44:23 +0530

    as

--- script.js
+++ script.js
@@ -33,3 +33,18 @@ function myFunction1(){
     moreText1.style.display = "inline";
   }
 }
+function myFunction2(){
+	var dots2 = document.getElementById("dots2");
+  var moreText2 = document.getElementById("more2");
+  var btnText2 = document.getElementById("myBtn2");
+  
+	  if (dots2.style.display === "none") {
+    dots2.style.display = "inline";
+    btnText2.innerHTML = "Read more";
+    moreText2.style.display = "none";
+  } else {
+    dots2.style.display = "none";
+    btnText2.innerHTML = "Read less";
+    moreText2.style.display = "inline";
+  }
+}
