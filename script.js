e2238d186b983fcef4fc184f2c59f972c89a290d
asking28 <asking28@gmail.com>
2020-10-08 17:32:56 +0530

    lkj

--- script.js
+++ script.js
@@ -3,9 +3,7 @@ function myFunction() {
   var moreText = document.getElementById("more");
   var btnText = document.getElementById("myBtn");
   
-  var dots1 = document.getElementById("dots1");
-  var moreText1 = document.getElementById("more1");
-  var btnText1 = document.getElementById("myBtn1");
+  
   
 
   if (dots.style.display === "none") {
@@ -18,7 +16,14 @@ function myFunction() {
     moreText.style.display = "inline";
   }
   
-  if (dots1.style.display === "none") {
+
+}
+function myFunction1(){
+	var dots1 = document.getElementById("dots1");
+  var moreText1 = document.getElementById("more1");
+  var btnText1 = document.getElementById("myBtn1");
+  
+	  if (dots1.style.display === "none") {
     dots1.style.display = "inline";
     btnText1.innerHTML = "Read more";
     moreText1.style.display = "none";
